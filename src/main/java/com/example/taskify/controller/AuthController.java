7516275bb2e973722a89444f94c4e9258de07d9a
KaShTaN27 <dkachan.lida@mail.ru>
2022-02-20 14:24:31 +0300

    Added login endpoint

--- src/main/java/com/example/taskify/controller/AuthController.java
+++ src/main/java/com/example/taskify/controller/AuthController.java
@@ -4,15 +4,19 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import com.example.taskify.controller.form.LoginForm;
 import com.example.taskify.controller.form.RegistrateOrganizationForm;
 import com.example.taskify.domain.AppUser;
 import com.example.taskify.domain.Organization;
 import com.example.taskify.domain.Role;
+import com.example.taskify.security.JwtTokenProvider;
 import com.example.taskify.service.OrganizationService;
 import com.example.taskify.service.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
@@ -34,6 +38,19 @@ public class AuthController {
 
     private final UserService userService;
     private final OrganizationService organizationService;
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenProvider tokenProvider;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody LoginForm form) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(form.getEmail(), form.getPassword()));
+        AppUser user = userService.getUser(form.getEmail());
+        String token = tokenProvider.generateToken(form.getEmail(), user.getRoles());
+        Map<Object, Object> response = new HashMap<>();
+        response.put("email", form.getEmail());
+        response.put("token", token);
+        return ResponseEntity.ok(response);
+    }
 
     @PostMapping("/signup")
     public ResponseEntity<?> registrateNewOrganization(@RequestBody RegistrateOrganizationForm form) {
