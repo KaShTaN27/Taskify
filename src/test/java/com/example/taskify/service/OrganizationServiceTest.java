2ba875dde1a71b7683eae403618202bbfe208b42
KaShTaN27 <dkachan.lida@mail.ru>
2022-03-01 03:02:54 +0300

    init OrganizationServiceTest class

---
+++ src/test/java/com/example/taskify/service/OrganizationServiceTest.java
@@ -0,0 +1,22 @@
+package com.example.taskify.service;
+
+import org.junit.jupiter.api.Test;
+
+class OrganizationServiceTest {
+
+    @Test
+    void saveOrganization() {
+    }
+
+    @Test
+    void getOrganization() {
+    }
+
+    @Test
+    void updateOrganizationById() {
+    }
+
+    @Test
+    void deleteOrganization() {
+    }
+}
