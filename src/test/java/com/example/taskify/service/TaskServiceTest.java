872c660707a43b196d01ed1179dffa1bf0d0c3cf
KaShTaN27 <dkachan.lida@mail.ru>
2022-03-02 19:04:50 +0300

    init TaskServiceTest

---
+++ src/test/java/com/example/taskify/service/TaskServiceTest.java
@@ -0,0 +1,36 @@
+package com.example.taskify.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskServiceTest {
+
+    @Test
+    void saveTask() {
+    }
+
+    @Test
+    void getTask() {
+    }
+
+    @Test
+    void getTaskById() {
+    }
+
+    @Test
+    void updateTaskById() {
+    }
+
+    @Test
+    void deleteTaskById() {
+    }
+
+    @Test
+    void addTaskToUsers() {
+    }
+
+    @Test
+    void createTaskAndSendEmail() {
+    }
+}
