a4d591a40cef86b37cd051da344f005faf713d23
KaShTaN27 <dkachan.lida@mail.ru>
2022-03-15 00:48:26 +0300

    restyling login page

--- src/main/frontend/src/pages/Login.js
+++ src/main/frontend/src/pages/Login.js
@@ -4,7 +4,7 @@ import {BASE_URL, getToken, saveEmail, savePersonalData, saveToken} from "../uti
 import jwtDecode from "jwt-decode";
 import {useNavigate} from "react-router-dom";
 
-export const Login = (props) => {
+export const Login = () => {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
@@ -23,7 +23,6 @@ export const Login = (props) => {
         })
     }
 
-
     const handleLogin = async () => {
         setError('');
         await axios.post(BASE_URL + "/api/auth/login", {
@@ -37,47 +36,49 @@ export const Login = (props) => {
 
             getUserInfo()
         }).catch(error => {
-            // if (error.response.status === 401 || error.response.status === 400) {
-            //     setError("Password or email is wrong.")
-            //     // setError(error.response.headers.get("Message"));
-            // } else {
-            //     setError("Something went wrong. Please, try again later.");
-            // }
             console.log('error >>> ', error)
         })
 
     }
 
     return (
-        <div className="login-page">
-            <div align="center" className="pb-3">
-                <h4>Welcome to the login page!</h4>
-                <hr />
+        <div className="row m-auto">
+            <div className="col-lg-6 mt-5" align="center">
+                <img src="https://account.bulletprofit.com/upload/auth/login.png" alt="login illustration"
+                     width="600px"/>
             </div>
-            <div className="mb-3">
-                <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Email"
-                    value={email}
-                    onChange={e => setEmail(e.target.value)}
-                />
+            <div className="col-lg-6">
+                <div className="login-page">
+                    <div align="center" className="pb-3">
+                        <h4>Welcome to the login page!</h4>
+                        <hr/>
+                    </div>
+                    <div className="mb-3">
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="Email"
+                            value={email}
+                            onChange={e => setEmail(e.target.value)}
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <input
+                            type="password"
+                            className="form-control"
+                            placeholder="Password"
+                            value={password}
+                            onChange={e => setPassword(e.target.value)}
+                        />
+                    </div>
+                    {error && <div className="error">{error}</div>}
+                    <div align="center">
+                        <button className="btn btn-primary" onClick={handleLogin}>Login</button>
+                    </div>
+                    <hr/>
+                    <a href="/">Registrate new organization</a>
+                </div>
             </div>
-            <div className="mb-3">
-                <input
-                    type="password"
-                    className="form-control"
-                    placeholder="Password"
-                    value={password}
-                    onChange={e => setPassword(e.target.value)}
-                />
-            </div>
-            {error && <div className="error">{error}</div>}
-            <div align="center">
-                <button className="btn btn-primary" onClick={handleLogin}>Login</button>
-            </div>
-            <hr/>
-            <a href="/">Registrate new organization</a>
         </div>
     )
 }
