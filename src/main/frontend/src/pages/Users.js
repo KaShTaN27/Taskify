5461b68e5fbcf22c59f7203c1b479a2006dffac8
KaShTaN27 <dkachan.lida@mail.ru>
2022-03-14 17:32:57 +0300

    change table to accordion

--- src/main/frontend/src/pages/Users.js
+++ src/main/frontend/src/pages/Users.js
@@ -1,7 +1,24 @@
 import React, {useEffect, useState} from "react";
-import {BASE_URL, getToken, getOrganizationName} from "../utils/Common";
+import {BASE_URL, getOrganizationName, getToken} from "../utils/Common";
 import axios from "axios";
 import jwtDecode from "jwt-decode";
+import {
+    Accordion,
+    AccordionActions,
+    AccordionDetails,
+    AccordionSummary,
+    Button,
+    FormControl,
+    Input,
+    InputAdornment,
+    InputLabel,
+    List,
+    ListItem,
+    ListItemIcon,
+    ListItemText,
+    Typography
+} from "@mui/material";
+import {AccountBox, Email, ExpandMoreOutlined, Hotel, Work} from "@mui/icons-material";
 
 export const Users = () => {
 
@@ -12,6 +29,66 @@ export const Users = () => {
     const [users, setUsers] = useState([]);
     const showUserCreationInterface = (jwtDecode(getToken()).roles.includes("ROLE_ADMIN"));
     const organization = getOrganizationName();
+    const [tasks, setTasks] = useState([]);
+
+    const [currentEmail, setCurrentEmail] = useState("");
+    const [newEmail, setNewEmail] = useState("");
+    const [selectedUser, setSelectedUser] = useState(-1);
+
+    const selectUser = (panel) => (event, newUser) => {
+        setSelectedUser(newUser ? panel : -1);
+        // Проверка на раскрытие accordion'a
+        if (selectedUser !== panel) {
+            getTasksOfUser(panel);
+            getUserById(panel);
+            setNewEmail(currentEmail);
+        }
+    }
+
+
+
+    const deleteUserById = (id) => {
+        const newUsers = users.filter(user => user.id !== id);
+        setUsers(newUsers);
+        axios.delete(`${BASE_URL}/api/user/${id}`, {
+            headers: {
+                'Authorization': getToken()
+            }
+        }).then(response => {
+            console.log(response.data)
+        }).catch(err => {
+            console.log(err)
+        })
+    }
+
+    const getUserById = (id) => {
+        axios.get(`${BASE_URL}/api/user/${id}`, {
+            headers: {
+                'Authorization': getToken()
+            }
+        }).then(response => {
+            console.log('User by id >>', response)
+            // TODO: setting currentEmail late
+            setCurrentEmail(response.data.email)
+            console.log("After setting", currentEmail)
+        }).catch(error => {
+            console.log('User by id error >>', error)
+        })
+
+    }
+
+    const getTasksOfUser = (id) => {
+        axios.get(`${BASE_URL}/api/user/${id}/tasks`, {
+            headers: {
+                'Authorization': getToken()
+            }
+        }).then(response => {
+            console.log('User by id >>', response)
+            setTasks(response.data)
+        }).catch(error => {
+            console.log('User by id error >>', error)
+        })
+    }
 
     const handleCreate = () => {
         axios.post(BASE_URL + "/api/user/create", {
@@ -26,10 +103,6 @@ export const Users = () => {
             }
         }).then(response => {
             console.log('Add task resp > ', response)
-            // setName('');
-            // setLastname('');
-            // setEmail('');
-            // setPassword('');
             window.location.reload();
         }).catch(err => {
             console.log('Add user err > ', err)
@@ -115,36 +188,64 @@ export const Users = () => {
                     <hr/>
                 </div>
             )}
-            <div>
-                <table className="table table-striped">
-                    <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">First name</th>
-                        <th scope="col">Last name</th>
-                        <th scope="col">Email</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    {users.length ? (users.map((user, index) => (
-                            <tr key={user.id}>
-                                <th scope="row">{index + 1}</th>
-                                <td>{user.firstName}</td>
-                                <td>{user.lastName}</td>
-                                <td>{user.email}</td>
-                            </tr>
-                        ))
-                    ) : (
-                        <tr>
-                            <th scope="row"> </th>
-                            <td> </td>
-                            <td> </td>
-                            <td> </td>
-                        </tr>
-                    )}
-
-                    </tbody>
-                </table>
+            <div className="add-user">
+                {users.map((user) => (
+                    <Accordion expanded={selectedUser === user.id} onChange={selectUser(user.id)} key={user.id}>
+                        <AccordionSummary expandIcon={<ExpandMoreOutlined/>}>
+                            <AccountBox sx={{width: '5%', color: 'text.secondary'}}/>
+                            <Typography sx={{width: '50%', flexShrink: 0}}>{user.firstName} {user.lastName}</Typography>
+                            <Typography sx={{color: 'text.secondary'}}>Email: {user.email}</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <FormControl variant="standard">
+                                <InputLabel htmlFor="input-with-icon-adornment">
+                                    Email
+                                </InputLabel>
+                                <Input
+                                    onChange={e => setNewEmail(e.target.value)}
+                                    // value=
+                                    defaultValue={user.email}
+                                    id="input-with-icon-adornment"
+                                    startAdornment={
+                                        <InputAdornment position="start">
+                                            <Email />
+                                        </InputAdornment>
+                                    }
+                                />
+                            </FormControl>
+                            <List>
+                                {tasks.length ? (tasks.map(task => (
+                                        <ListItem key={task.id}>
+                                            <ListItemIcon>
+                                                <Work/>
+                                            </ListItemIcon>
+                                            <ListItemText>
+                                                <Typography>{task.title}</Typography>
+                                                <small>Deadline: {task.deadline}</small>
+                                            </ListItemText>
+                                        </ListItem>
+                                    )))
+                                    : (
+                                        <ListItem>
+                                            <ListItemIcon>
+                                                <Hotel/>
+                                            </ListItemIcon>
+                                            <ListItemText>
+                                                The user has no tasks
+                                            </ListItemText>
+                                        </ListItem>
+                                    )}
+                            </List>
+                        </AccordionDetails>
+                        <AccordionActions>
+                            <Button variant="outlined" color="success"
+                            disabled={(newEmail === user.email) || (newEmail === "")}>Update</Button>
+                            <Button variant="outlined" color="error"
+                                    onClick={() => deleteUserById(user.id)}>Delete</Button>
+                        </AccordionActions>
+                    </Accordion>
+                ))
+                }
             </div>
         </div>
     )
